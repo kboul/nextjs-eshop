88e6b4f991e1a08b34035a0b48082740ab9792b8
kboul <kos.voulgaridis@gmail.com>
2025-06-26 23:22:42 +0300

    Styles for main layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"]
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"]
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className="flex min-h-full flex-col bg-white">
         <Navbar />
-        <main>{children}</main>
+        <main className="flex-grow container mx-auto px-4 py-8">{children}</main>
       </body>
     </html>
   );
