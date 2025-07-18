e735d867685b09834741b6d7a80d5c79d63f2184
kboul <kos.voulgaridis@gmail.com>
2025-07-18 22:54:03 +0300

    Tell ts to ignore any on User type cause it is not exported from lib

--- src/utils/admin.ts
+++ src/utils/admin.ts
@@ -1,6 +1,7 @@
 const getAdminEmails = () =>
   process.env.NEXT_PUBLIC_ADMIN_EMAILS ? process.env.NEXT_PUBLIC_ADMIN_EMAILS.split(",") : [];
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const getIsAdmin = (isSignedIn: boolean | undefined, user: any | null | undefined, adminEmails: string[]) => {
   return isSignedIn && user?.primaryEmailAddress?.emailAddress
     ? adminEmails.includes(user.primaryEmailAddress.emailAddress)
